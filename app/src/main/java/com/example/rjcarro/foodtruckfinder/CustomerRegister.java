36e0de8bed3ae92d0d7dff1dee895c22bd900c41
Carroll <jake.carroll@okstate.edu>
2019-04-04 16:49:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rjcarro/foodtruckfinder/CustomerRegister.java
@@ -0,0 +1,13 @@
+package com.example.rjcarro.foodtruckfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_register);
+    }
+}
